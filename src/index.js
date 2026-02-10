f06181b7f36a23f24b1665f9c7920f34841b07dd
= <=>
2026-02-09 16:49:57 -0800

    init

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import express from 'express'
+
+const app = express()
+
+app.get('/',async(req,res)=>{
+    res.status(200).send({"message":"Welcome to server health 10/10"})
+})
+
+app.listen(8000,()=>{
+    console.log('server is running on http://localhost:8000');
+    
+})
