fcf85713cf6bc7546491a7a37c6119264d258ee2
= <=>
2026-02-13 14:11:34 -0800

    added api end point for creating /getting matches

--- src/routes/matches.js
+++ src/routes/matches.js
@@ -1,13 +1,32 @@
 import express from 'express'
-import {createMatchSchema} from '../validation/validation.js'
+import {createMatchSchema, listMatchesQuerySchema} from '../validation/validation.js'
 import {db} from '../db/db.js'
 import {matches} from '../db/schema.js'
 const matchesrouter = express.Router()
 import {MATCH_STATUS} from '../validation/validation.js'
 import { getMatchStatus } from '../utils/match-status.js'
+import { desc } from 'drizzle-orm'
 
+const Max_limit = 100
 matchesrouter.get('/',async(req,res)=>{
-    res.status(200).send({message:"hello from message route"})
+   const parsed = listMatchesQuerySchema.safeParse(req.query)
+   if(!parsed.success){
+    return res.status(400).send({error:"invalid query",detail:JSON.stringify(parsed.error)})
+ }
+
+ const limit =  Math.min(parsed.data.limit ?? 50,Max_limit)
+
+
+    try {
+        const data = await db
+        .select()
+        .from(matches)
+        .orderBy((desc(matches.createdAt)))
+        .limit(limit)
+        res.status(200).send({data})
+    } catch (error) {
+        res.status(500).send({error:'failed to get matches',data:error})
+    }
 })
 matchesrouter.post('/data', async(req,res)=>{
     console.log(req.body ,"this is req.body😊")
